9eba32a1e350a37f1c556164503c33d6396671c8
Manoel NUNES BORBA <76746287+manoelnunesborba@users.noreply.github.com>
2024-01-19 16:07:30 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php
+
 echo "Hello from your first test done in CI and github actions ";
-echo "<br>"; 
+echo "<br>";
 echo "Please answer the following questions ";
 echo "<br>";
-echo 'question1'; 
+echo 'question1';
 echo "<br>";
-echo 'question2'; 
+echo 'question2';
 echo "<br>";
-echo 'question3'; 
+echo 'question3';
 echo "<br>";
-?>
