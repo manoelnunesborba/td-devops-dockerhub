03d59cae1383746a95b3d2efbfd7d643d5aea9f0
Manoel NUNES BORBA <76746287+manoelnunesborba@users.noreply.github.com>
2024-01-19 15:45:04 +0100

    fix lint

--- choise.py
+++ choise.py
@@ -46,16 +46,16 @@ words = [
 ]
 
 
-firstquestion = random.randint(0,5)
+firstquestion = random.randint(0, 5)
 secondquestion = firstquestion
 
 while secondquestion == firstquestion:
-  secondquestion = random.randint(0,5)
+  secondquestion = random.randint(0, 5)
 
 thirdquestion = firstquestion
 
 while thirdquestion == firstquestion or thirdquestion == secondquestion:
-  thirdquestion = random.randint(0,5)
+  thirdquestion = random.randint(0, 5)
 
 print("Firstquestion :")
 print(words[firstquestion])
@@ -64,13 +64,13 @@ print(words[secondquestion])
 print("Thirdquestion :")
 print(words[thirdquestion])
 
-#input file
+# input file
 fin = open("data.txt", "rt")
-#output file to write the result to
+# output file to write the result to
 fout = open("index.php", "wt")
-#for each line in the input file
+# for each line in the input file
 for line in fin:
-    fout.write(line.replace('question1', words[firstquestion]).replace('question2', words[secondquestion]).replace('question3', words[thirdquestion]))
-#close input and output files
+    fout.write(line.replace("question1", words[firstquestion]).replace("question2", words[secondquestion]).replace("question3", words[thirdquestion]))
+# close input and output files
 fin.close()
 fout.close()
